8afa9b61f8354126978f592a7d078037df7c27af
Marija Ilić <marija.cbsqg@gmail.com>
2024-08-05 12:16:56 +0200

    PDO First Steps

--- dynamic-web-applications/index.php
+++ dynamic-web-applications/index.php
@@ -1,4 +1,29 @@
 <?php
 
     require "functions.php";
-    require "router.php";
+    // require "router.php";
+
+    // dsn: data source name - connection string
+    $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+    $username = 'root';
+    $password = 'root';
+
+    // PDO: PHP Data Objects
+    // -------------------------------------------------------------
+    // database access layer koji pruza uniformne metode
+    // za pristup bazama podataka
+    // -------------------------------------------------------------
+    // mogu koristiti iste funkcije za komunikaciju bez obzira
+    // na bazu podataka koju koristim
+
+    $pdo = new PDO($dsn, $username, $password);
+
+    $statement = $pdo->prepare("select * from posts");
+
+    $statement->execute();
+
+    $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($posts as $post) {
+        echo "<li>" . $post['title'] . "</li>";
+    }
